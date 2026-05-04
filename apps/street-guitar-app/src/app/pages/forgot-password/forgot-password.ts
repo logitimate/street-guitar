e4eb81a6b7a394ce70e47cc7857b7e7eb08118ae
Logan Livingston <lrlivingston@live.com>
2026-05-03 23:43:46 -0500

    Fixing build issue

--- apps/street-guitar-app/src/app/pages/forgot-password/forgot-password.ts
+++ apps/street-guitar-app/src/app/pages/forgot-password/forgot-password.ts
@@ -1,6 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,20 +7,4 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './forgot-password.css',
   imports: [RouterLink],
 })
-export class ForgotPassword {
-  private auth = inject(AuthService);
-
-  loading = signal(false);
-  sent = signal(false);
-  sentTo = signal('');
-
-  async submit(emailInput: HTMLInputElement) {
-    const email = emailInput.value.trim();
-    if (!email) return;
-    this.loading.set(true);
-    await this.auth.sendPasswordReset(email);
-    this.loading.set(false);
-    this.sentTo.set(email);
-    this.sent.set(true);
-  }
-}
+export class ForgotPassword {}
